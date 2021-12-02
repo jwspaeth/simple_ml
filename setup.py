f278fd8bf78ffb07c8e5d3caa9acc2a7d7542828
Will Spaeth <jwspaeth0@gmail.com>
2021-12-02 12:36:43 -0600

    Fix script again.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_namespace_packages, setup
 
 
-setup(name="simple_ml",	version=".1",
+setup(name="simple_ml",	version="0.1",
     author="Will Spaeth",
     author_email="jwspaeth0@gmail.com",
     packges=find_namespace_packages(include=["simple_ml", "hydra_plugins.*"]),
-    scripts=["scripts/simple_train", "scripts/simple_test"],
+    scripts=["scripts/simple_train.py", "scripts/simple_test.py"],
     install_requires=[
         "hydra",
         "hydra-submitit-launcher",
