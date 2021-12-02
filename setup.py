7ef3101912efc44e80cb65886e0ccb1b51f12da4
Will Spaeth <jwspaeth0@gmail.com>
2021-12-02 12:34:20 -0600

    Testing new script

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name="simple_ml",	version=".1",
     author="Will Spaeth",
     author_email="jwspaeth0@gmail.com",
     packges=find_namespace_packages(include=["simple_ml", "hydra_plugins.*"]),
-    scripts=["scripts/simple_train.py", "scripts/simple_test.py"],
+    scripts=["scripts/simple_train", "scripts/simple_test"],
     install_requires=[
         "hydra",
         "hydra-submitit-launcher",
