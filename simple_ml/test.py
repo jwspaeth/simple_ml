e949113308411a9de7d46164d324b858490b50d1
Will Spaeth <jwspaeth0@gmail.com>
2021-12-06 21:21:20 -0600

    Adding ckpt_path

--- simple_ml/test.py
+++ simple_ml/test.py
@@ -3,6 +3,6 @@ from hydra.utils import instantiate
 from simple_ml.utils import setup_ml
 
 
-def test(**kwargs):
+def test(ckpt_path=None, **kwargs):
     data_dict, model, trainer = setup_ml(**kwargs)
-    trainer.test(model, **data_dict)
+    trainer.test(model, ckpt_path=ckpt_path, **data_dict)
--- simple_ml/train.py
+++ simple_ml/train.py
@@ -3,9 +3,9 @@ from hydra.utils import instantiate
 from simple_ml.utils import setup_ml
 
 
-def train(return_validation_result=True, **kwargs):
+def train(return_validation_result=True, ckpt_path=None, **kwargs):
     data_dict, model, trainer = setup_ml(**kwargs)
-    trainer.fit(model, **data_dict)
+    trainer.fit(model, ckpt_path=ckpt_path, **data_dict)
 
     if return_validation_result:
         return 0
